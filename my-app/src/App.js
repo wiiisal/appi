1976a452639edb8a273390a0c206aed769ab0328
Wiiisal <wissalbensai011@gmail.com>
2022-12-12 10:14:40 +0100

    react checkpoint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Use from './Use';
+import Tableau from './tableau';
+import Top from './top';
 
 function App() {
   return (
     <div className="App">
+      <Top/>
       <Use/>
+      <Tableau/>
     </div>
   );
 }
